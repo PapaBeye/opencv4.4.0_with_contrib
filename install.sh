e2400c64bacd157b7f88dc6e5aba583759f5a314
Papa Beye <beyep@vcu.edu>
2021-04-05 03:05:35 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,2 +1,25 @@
-TODO
+echo "** Install requirement"
+sudo apt-get update
+sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+sudo apt-get install -y curl
+sudo apt-get update
+
+unzip opencv-4.4.0.zip 
+unzip opencv_contrib-4.4.0.zip 
+cd opencv-4.4.0/
+
+echo "** Building..."
+mkdir release
+cd release/
+cmake -D WITH_CUDA=ON -D ENABLE_PRECOMPILED_HEADERS=OFF  -D CUDA_ARCH_BIN="7.2" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.4.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=../.. ..
+make -j$(nproc)
+sudo make install
+
+cp -r 
+
+echo "** Install opencv-4.4.0 successfully"
 
